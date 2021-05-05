b7e6a1e00346849743c8c6f3ff6a2745398bf38b
lauretteroy <laurieroy.dev@gmail.com>
2021-05-05 10:15:51 -0600

    Add Shopping basket

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,17 +9,49 @@
 						<th>Add to basket</th>
 					</tr>
 				</thead>
-				<tbody v-for=" item in getMenuItems">
+				<tbody v-for="item in getMenuItems">
 					<tr>
 						<td><strong>{{ item.name }}</strong></td>
 					</tr>
 					<tr v-for="option in item.options">
 						<td>{{ option.size }}</td>
 						<td>{{ option.price }}</td>
-						<td><button class="btn btn-sm btn-outline-success" type="button">+</button></td>
+						<td><button class="btn btn-sm btn-outline-success" 
+												type="button"
+												@click="addToBasket( item, option )"
+												>+</button></td>
 					</tr>
 				</tbody>
 			</table>
+		</div>
+
+		<!-- shopping basket -->
+		<div class="col-sm-12 col-md-6">
+			<div v-if="basket.length > 0">
+				<table class="table">
+					<thead class="thead-default">
+						<tr>
+							<th>Quantity</th>
+							<th>Item</th>
+							<th>Total</th>
+						</tr>
+					</thead>
+					<tbody v-for="item in basket">
+						<tr>
+							<td><button class="btn btn-sm" type="button">-</button>
+							<span>{{ item.quantity }}</span>
+							<button class="btn btn-sm" type="button">+</button></td>
+							<td>{{ item.name }} {{ item.size }}"</td>
+							<td>{{ item.price * item.quantity }}</td>
+						</tr>
+					</tbody>
+				</table>
+				<p>Order total: </p>
+				<button class="btn btn-success btn-block">Place Order</button> 
+			</div>
+			<div v-else>
+				<p>{{basketText}}</p>
+			</div>
 		</div>
 	</div>
 </template>
@@ -28,7 +60,10 @@
 	export default {
 		data() {
 			return {
-				getMenuItems: {
+				basket: [],
+				basketText: "Your basket is empty!",
+				getMenuItems: 
+				{
         	1: {
            name: "Margherita",
            description: "A delicious tomato based pizza topped with mozzarella",
@@ -74,6 +109,16 @@
 						]
 					}
 				}	
+			}
+		},
+		methods: {
+			addToBasket(item, option) {
+				this.basket.push({
+					name: item.name,
+					price: option.price,
+					size: option.size,
+					quantity: 1
+				})
 			}
 		}
 	}
