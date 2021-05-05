d73cd3c9a79ff4da66fca47f5f65bf099d5d555c
lauretteroy <laurieroy.dev@gmail.com>
2021-05-05 11:08:35 -0600

    Add form bindings

--- src/components/NewPizza.vue
+++ src/components/NewPizza.vue
@@ -4,45 +4,71 @@
 		<div class="form-group row">
 			<label for="" class="col-sm-3">Name</label>
 			<div class="col-sm-9">
-				<input type="text" class="form-control">
+				<input type="text" class="form-control" v-model="newPizza.name">
 			</div>
 		</div>
 		<div class="form-group row">
 			<label for="" class="col-sm-3">Description</label>
 			<div class="col-sm-9">
-				<textarea type="text" class="form-control" rows="5"></textarea>
+				<textarea type="text" class="form-control" rows="5" v-model="newPizza.description"></textarea>
 			</div>
 		</div>
 		<p><strong>Option 1:</strong></p>
 		<div class="form-group row">
 			<label for="" class="col-sm-3">Size (")</label>
 			<div class="col-sm-9">
-				<input type="text" class="form-control">
+				<input type="text" class="form-control"  v-model="newPizza.options[0].size">
 			</div>
 		</div>
 		<div class="form-group row">
 			<label for="" class="col-sm-3">Price ($)</label>
 			<div class="col-sm-9">
-				<input type="text" class="form-control">
+				<input type="text" class="form-control"  v-model="newPizza.options[0].price">
 			</div>
 		</div>
 		<p><strong>Option 2:</strong></p>
 		<div class="form-group row">
 			<label for="" class="col-sm-3">Size (")</label>
 			<div class="col-sm-9">
-				<input type="text" class="form-control">
+				<input type="text" class="form-control"  v-model="newPizza.options[1].size">
 			</div>
 		</div>
 		<div class="form-group row">
 			<label for="" class="col-sm-3">Price ($)</label>
 			<div class="col-sm-9">
-				<input type="text" class="form-control">
+				<input type="text" class="form-control" v-model="newPizza.options[1].price">
 			</div>
 		</div>
 		<div class="form-group row">
-			<button class="btn btn-success btn-block" type="button">Add</button>
+			<button class="btn btn-success btn-block" type="button" @click="addMenuItem">Add</button>
 		</div>
 	</form>
 </template>
-<script></script>
+<script>
+export default {
+	data() {
+		return {
+			newPizza: {
+				 name: "E.g., Margherita",
+           description: "E.g., A delicious tomato based pizza topped with mozzarella",
+           options: [
+             {
+               size: 9,
+               price: 6.95
+             },
+             {
+               size: 12,
+               price: 10.95
+						 }
+					 ]
+			}
+		}
+	},
+	methods: {
+		addMenuItem() {
+			console.log(this.newPizza)
+		},
+	}
+}
+</script>
 <style></style>
