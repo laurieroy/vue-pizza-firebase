65ce3d4dc7760fdcc43cf8a4f7369f2fdb9c720b
lauretteroy <laurieroy.dev@gmail.com>
2021-05-10 17:34:27 -0600

    refactor to modules

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,1 @@
-export const currentUser = state => state.currentUser
-export const getMenuItems = state => state.menuItems
-export const numberOfOrders = state => state.orders.length
+
--- src/store/store.js
+++ src/store/store.js
@@ -1,65 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import * as actions from './actions'
-import * as getters from './getters'
-import * as mutations from './mutations'
+import menu from './modules/menu'
+import orders from './modules/orders'
+import users from './modules/users'
 
 Vue.use(Vuex)
 
-export const store = new Vuex.store({
-	state: {
-		menuItems: 
-		{
-			1: {
-			 name: "Margherita",
-			 description: "A delicious tomato based pizza topped with mozzarella",
-			 options: [
-				 {
-					 size: 9,
-					 price: 6.95
-				 },
-				 {
-					 size: 12,
-					 price: 10.95
-				 }
-			 ]
-			},
-			2: {
-				name: "Pepperoni",
-				description:
-					"A delicious tomato based pizza topped with mozzarella and pepperoni",
-				options: [
-					{
-						size: 9,
-						price: 7.95
-					},
-					{
-						size: 12,
-						price: 12.95
-					}
-				]
-			},
-			3: {
-				name: "Hawaiian",
-				description:
-					"A delicious tomato based pizza topped with mozzarella, ham and pineapple",
-				options: [
-					{
-						size: 9,
-						price: 7.95
-					},
-					{
-						size: 12,
-						price: 12.95
-					}
-				]
-			}
-		},
-		orders: [],
-		currentUser: null
-	},
-	actions,
-	getters,
-	mutations
+export const store = new Vuex.Store({
+	modules: {
+		menu,
+		orders,
+		users
+	}
 })
