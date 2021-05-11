cdbd5d6755eea6b4752caa06503ec0f80aafcfbd
lauretteroy <laurieroy.dev@gmail.com>
2021-05-10 18:25:06 -0600

    Hide admin if user not logged in

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,57 +1,60 @@
 <template>
 	<div>
-		<div class="row">
-			<div class="col-sm-12 col-md-6">
-				<pp-new-pizza></pp-new-pizza>
+		<section v-if="currentUser">
+			<div class="row">
+				<div class="col-sm-12 col-md-6">
+					<pp-new-pizza></pp-new-pizza>
+				</div>
+				<div class="col-sm-12 col-md-6">
+					<h3>Menu:</h3>
+					<table class="table table-hover">
+						<thead class="thead-default">
+							<tr>
+								<th>Item</th>
+								<th>Remove from menu</th>
+							</tr>
+						</thead>
+						<tbody v-for="item in getMenuItems" :key="item['.key']">
+							<tr>
+								<td>{{item.name}}</td>
+								<td><button class="btn btn-sm btn-outline-danger"
+									@click="removeMenuItem(item['.key'])">x</button></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 			</div>
-			<div class="col-sm-12 col-md-6">
-				<h3>Menu:</h3>
-				<table class="table table-hover">
-					<thead class="thead-default">
-						<tr>
-							<th>Item</th>
-							<th>Remove from menu</th>
-						</tr>
-					</thead>
-					<tbody v-for="item in getMenuItems" :key="item['.key']">
-						<tr>
-							<td>{{item.name}}</td>
-							<td><button class="btn btn-sm btn-outline-danger"
-								@click="removeMenuItem(item['.key'])">x</button></td>
-						</tr>
-					</tbody>
-				</table>
+			<div class="row">
+				<div class="col-sm-12">
+					<h3>Current Orders: {{numberOfOrders}} </h3>
+					<table class="table table-sm" v-for="(orders, index) in getOrders"  :key="orders['.key']>
+						<thead class="thead-default">
+							<tr>
+								<th>Item</th>
+								<th>Size</th>
+								<th>Quantity</th>
+								<th>Price</th>
+							</tr>
+						</thead>
+						<tbody>
+							<div class="order-number">
+								<strong><em>Order Number: {{ index +1 }}</em></strong>
+								<button class="btn btn-sm btn-outline-danger"
+									@click="removeOrderItem(orders['.key'])"
+								>x</button>
+							</div>
+							<tr v-for="orderItems in orders['.value']">
+								<td>{{ orderItems.name }}</td>
+								<td>{{ orderItems.size }}</td>
+								<td>{{ orderItems.quantity }}</td>
+								<td>{{ orderItems.price }}</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-12">
-				<h3>Current Orders: {{numberOfOrders}} </h3>
-				<table class="table table-sm" v-for="(orders, index) in getOrders"  :key="orders['.key']>
-					<thead class="thead-default">
-						<tr>
-							<th>Item</th>
-							<th>Size</th>
-							<th>Quantity</th>
-							<th>Price</th>
-						</tr>
-					</thead>
-					<tbody>
-						<div class="order-number">
-							<strong><em>Order Number: {{ index +1 }}</em></strong>
-							<button class="btn btn-sm btn-outline-danger"
-								@click="removeOrderItem(orders['.key'])"
-							>x</button>
-						</div>
-						<tr v-for="orderItems in orders['.value']">
-							<td>{{ orderItems.name }}</td>
-							<td>{{ orderItems.size }}</td>
-							<td>{{ orderItems.quantity }}</td>
-							<td>{{ orderItems.price }}</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
+		</section>
+
 		<hr>
 		<div class="row">
 			<div class="col-sm-12 col-lg-6">
@@ -64,7 +67,7 @@
 <script>
 import NewPizza from './NewPizza';
 import Login from './Login';
-import { mapGetters } from 'vuex'
+import { mapGetters } from 'vuex';
 import { dbMenuRef, dbOrdersRef } from '../firebaseConfig';
 
 export default {
@@ -74,6 +77,7 @@ export default {
 	},
 	computed: {
 			...mapGetters([
+				'currentUser',
 				'numberOfOrders',
 				'getMenuItems',
 				'getOrders'
